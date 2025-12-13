8c50b0d17c1b3624ec5fbb03088564d7ad475628
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-12-13 14:30:07 +0600

    feat: user register validate schema

---
+++ quick_memo_api/src/schemas/userSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+  body: z.object({
+    name: z.string().min(3, "Name must be at least 3 characters long"),
+    email: z.string().email("Invalid email address"),
+    mobile: z
+      .string()
+      .min(11, "Mobile number must be at least 11 characters long")
+      .or(z.literal('')) // Allow an empty string
+      .transform((value) => value === '' ? null : value)  // Transform empty string to null
+      .optional()
+      .nullable(),  // Allows the final result to be null
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+  }),
+});
