7a86c49ced0335b2fedcd9af27cd1686625882db
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2026-01-16 19:11:28 +0600

    remove backend from here

--- quick_memo_api/src/db/migrations/010_add_shop_slug.sql
+++
@@ -1,6 +0,0 @@
--- Add shop_slug column to users table for unique public shop URLs
-ALTER TABLE users
-ADD COLUMN IF NOT EXISTS shop_slug VARCHAR(255) UNIQUE;
-
--- Create an index for faster lookups by slug
-CREATE INDEX IF NOT EXISTS idx_users_shop_slug ON users(shop_slug);
